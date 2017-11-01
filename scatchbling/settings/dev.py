9f5ec5cb8b9702b53ddb910f51ee0727c554b29f
CGick <chrisgick31@gmail.com>
2017-11-01 14:35:32 -0400

    Configure Dev settings for Heroku

--- scatchbling/settings/dev.py
+++ scatchbling/settings/dev.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w3dwyi!-)#o!d$%%srguy&03uxxn*i8m2#=0yd*f$9ajb$da=9'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
