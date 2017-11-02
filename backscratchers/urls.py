c709dba4733a77ca18835a987707642fbc732047
CGick <chrisgick31@gmail.com>
2017-11-02 11:19:26 -0400

    Make api web browsable improve http status reporting

--- backscratchers/urls.py
+++ backscratchers/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls  import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from backscratchers import views
 
 
@@ -6,3 +7,5 @@ urlpatterns = [
     url(r'^backscratchers/$', views.backscratcher_list),
     url(r'^backscratchers/(?P<pk>[0-9]+)/$', views.backscratcher_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backscratchers/views.py
+++ backscratchers/views.py
@@ -1,59 +1,68 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
 from backscratchers.models import BackScratcher
 from backscratchers.serializers import BackScratcherSerializer
 
 
 @api_view(["GET", "POST"])
 @csrf_exempt
-def backscratcher_list(request):
+def backscratcher_list(request, format=None):
     """
     List all backscratchers, or create a new backscratcher.
     """
     if request.method == "GET":
         backscratchers = BackScratcher.objects.all()
         serializer = BackScratcherSerializer(backscratchers, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = BackScratcherSerializer(data=data)
+        serializer = BackScratcherSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 @api_view(["GET", "PUT", "DELETE"])
 @csrf_exempt
-def backscratcher_detail(request, pk):
+def backscratcher_detail(request, pk, format=None):
     """
     Retrieve, update, or delete a backscratcher.
     """
     try:
         backscatcher = BackScratcher.objects.get(pk=pk)
     except BackScratcher.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = BackScratcherSerializer(backscatcher)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = BackScratcherSerializer(backscatcher, data=data)
+        serializer = BackScratcherSerializer(
+            backscatcher,
+            data=request.data
+        )
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     elif request.method == "DELETE":
         backscatcher.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
